f5c2a5e7b136d588d89ba4d20f60494f4a747ea6
ivy-26 <i.li@ufl.edu>
2023-07-19 00:23:27 -0400

    Created main and encoder funtion.

---
+++ version_control.py
@@ -0,0 +1,53 @@
+# Ivy Li
+
+# Variable to store the encoded password
+stored_password = 0
+
+# Function to encode the password
+def encode(password):
+    encoded_password = ""
+    for char in password:
+        # Shift each digit up by 3 numbers
+        encoded_digit = str((int(char) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+# Main function to run the program
+def main():
+    global stored_password
+
+    while True:
+        # Display the menu options
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print(" ")
+
+        # Get user's choice
+        choice = input("Please enter an option: ")
+
+        if choice == "1":
+            # Encoding option
+            password = input("Please enter your password to encode: ")
+            stored_password = encode(password)
+            print("Your password has been encoded and stored!")
+            print(" ")
+
+        elif choice == "2":
+            # Decoding option
+            print("The encoded password is " + stored_password + ", and the original password is " + decode(stored_password) + ".")
+            print(" ")
+
+        elif choice == "3":
+            # Quit option
+            break
+
+        else:
+            # Invalid choice
+            print("Invalid choice.")
+
+# Run the main function if this script is executed directly
+if __name__ == "__main__":
+    main()
